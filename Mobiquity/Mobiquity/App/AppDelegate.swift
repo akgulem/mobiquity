685f62f51c96cafcd1e5a5b96b2a613586c14fd2
Emrah Akgül <emrah.akgul@vngrs.com>
2022-01-05 22:12:50 +0300

    change application entry point to dummy view controller

--- Mobiquity/Mobiquity/App/AppDelegate.swift
+++ Mobiquity/Mobiquity/App/AppDelegate.swift
@@ -9,10 +9,16 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
+
+    var window: UIWindow?
+    var navigationController: UINavigationController?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow()
+        let viewController = UIViewController()
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
         return true
     }
 }
