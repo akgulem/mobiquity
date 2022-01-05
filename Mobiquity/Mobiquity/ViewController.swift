f5814014446e7028b4c2bde700db03a794ec17bf
Emrah Akgül <emrah.akgul@vngrs.com>
2022-01-05 22:04:21 +0300

    create project

---
+++ Mobiquity/Mobiquity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mobiquity
+//
+//  Created by Emrah Akgül on 5.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
