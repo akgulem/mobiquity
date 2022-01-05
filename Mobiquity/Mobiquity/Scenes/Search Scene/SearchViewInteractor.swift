400764b6196c3bba6f825bce87670e26962fdc8e
Emrah Akgül <emrah.akgul@vngrs.com>
2022-01-06 00:21:34 +0300

    add search image service as a dependency to search view router

--- Mobiquity/Mobiquity/Scenes/Search Scene/SearchViewInteractor.swift
+++ Mobiquity/Mobiquity/Scenes/Search Scene/SearchViewInteractor.swift
@@ -6,8 +6,12 @@
 //
 
 import Foundation
+import Network
 
 protocol SearchViewInteractorInterface {
+
+    func search(with text: String)
+    func reset()
 }
 
 protocol SearchViewInteractorOutput: BaseInteractorOutput {
@@ -16,8 +20,18 @@ protocol SearchViewInteractorOutput: BaseInteractorOutput {
 final class SearchViewInteractor {
 
     weak var output: SearchViewInteractorOutput?
+    private var searchImagesService: SearchImagesServiceProtocol
+
+    init(searchImagesService: SearchImagesServiceProtocol) {
+        self.searchImagesService = searchImagesService
+    }
 }
 
 extension SearchViewInteractor: SearchViewInteractorInterface {
 
+    func search(with text: String) {
+    }
+
+    func reset() {
+    }
 }
