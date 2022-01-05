adae14f88e0da190a2b2c73d16a136a0ad6a6ba9
Emrah Akgül <emrah.akgul@vngrs.com>
2022-01-05 22:09:59 +0300

    add register and dequeue methods for collection view extension

---
+++ Mobiquity/Mobiquity/Extensions/UICollectionView+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  UICollectionView+Extension.swift
+//  Mobiquity
+//
+//  Created by Emrah Akgül on 5.01.2022.
+//
+
+import UIKit
+
+extension UICollectionView {
+
+    func register<T: UICollectionViewCell>(cellType: T.Type, bundle: Bundle? = nil) {
+        let className = String(describing: cellType.self)
+        let nib = UINib(nibName: className, bundle: bundle)
+        register(nib, forCellWithReuseIdentifier: className)
+    }
+
+    func dequeueReusableCell<T: UICollectionViewCell>(with type: T.Type,
+                                                             for indexPath: IndexPath) -> T? {
+        return dequeueReusableCell(withReuseIdentifier: String(describing: type.self), for: indexPath) as? T
+    }
+}
