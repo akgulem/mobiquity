5277a39124a81a879621399f685e8a52e33efef8
Emrah Akgül <emrah.akgul@vngrs.com>
2022-01-05 22:07:52 +0300

    create base folders for main project

---
+++ Mobiquity/Mobiquity/App/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Mobiquity
+//
+//  Created by Emrah Akgül on 5.01.2022.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
