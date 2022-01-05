4fc78f28372450f47420f268873b9993d6b3881e
Emrah Akgül <emrah.akgul@vngrs.com>
2022-01-05 22:23:51 +0300

    create network swift package and add it to project

--- Mobiquity/Mobiquity/App/AppDelegate.swift
+++ Mobiquity/Mobiquity/App/AppDelegate.swift
@@ -12,7 +12,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     var navigationController: UINavigationController?
-    
+
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
